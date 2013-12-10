b1d1d0109b885e7d5936a99caf40e1fafa95145b
sunil <snlkumar447@gmail.com>
2013-12-10 15:44:15 +0530

    j-query slidder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bootstrap::Application.config.secret_token = '92092907c758a32e6ac117210719e67b9bd38fe5a6c20e0e742d95b696d91f21a3732f219e695187b5874e2e38f86000e0a3304b7754f28b289dc3ad0b8b49dc'
